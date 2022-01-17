5d95275e3bfe0b8e697908b0f4769fe597752f99
dipnesadiya123 <dipkumar.nesadiya@smartsensesolutions.com>
2022-01-17 11:10:02 +0530

    Added listener to front-end

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'/js'));
 
 io.on('connection', () => {
     console.log('User connected via socket.io');
